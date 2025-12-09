36009c3676fffebaeff95e2c031ca70fd53607dc
Obezyan0941 <na_paramonov@mail.ru>
2025-12-09 12:10:11 +0300

    typization improved

--- src/components/app_state/app_state.tsx
+++ src/components/app_state/app_state.tsx
@@ -1,4 +1,5 @@
 import { createContext } from 'react';
+import type { AppState } from '../../types/appStateTypes';
 
-export const AppContext = createContext({ chat_list: []});
+export const AppContext = createContext<AppState>({chat_list: []});
 export const AppDispatchContext = createContext({});
